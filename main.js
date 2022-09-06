a5f1aedb759ac06a1174a4d502bd4e8d4630e688
{Mameta29} <{fc.kikkawa@gmail.com}>
2022-09-06 14:37:23 +0900

    add tokens 情報

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const [, , firstArg] = process.argv;
+const [, , firstArg, secondArg] = process.argv;
 
 require('dotenv').config();
 
@@ -7,16 +7,16 @@ const metadataCreate = require('./src/lib/metadataCreate');
 const { makeExecutionData } = require('./src/lib/makeLog');
 
 (async () => {
-	try {
-        if(firstArg == 'create'){
-            await metadataCreate.createMetadata ();
-            makeExecutionData('create metadata');
-        } else if(firstArg == 'update'){
-            metadataUpdate.update();
-        } else {
-            throw new Error('コマンドが違います。')
-        }
-	} catch(error){
-		console.error(error.message);
-	}
+  try {
+    if (firstArg == 'create' && secondArg != null) {
+      await metadataCreate.createMetadata();
+      makeExecutionData('create metadata');
+    } else if (firstArg == 'update') {
+      metadataUpdate.update();
+    } else {
+      throw new Error('コマンドが違います。');
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 })();
--- src/lib/metadataCreate.js
+++ src/lib/metadataCreate.js
@@ -6,173 +6,276 @@ const { makeExecutionData } = require('./makeLog');
 
 const { Client } = require('@notionhq/client');
 
+// alchemy sdkをimport
+const { Network, Alchemy } = require('alchemy-sdk');
+
 const client = new Client({ auth: process.env.WAGUMI_SBT_API_TOKEN });
 
-const contributions = []
+const contributions = [];
+
+// tokens情報取得のための準備
+const ownerAddr = '0x7DcF131D0f8Bec6CaEa72d41Da9dB3dC4845644D';
+// const ownerAddr = '';
+let nftsForOwner;
+const chains = [
+  {
+    chainId: 1,
+    network: Network.ETH_MAINNET,
+  },
+  {
+    chainId: 3,
+    network: Network.ETH_ROPSTEN,
+  },
+  {
+    chainId: 4,
+    network: Network.ETH_RINKEBY,
+  },
+  {
+    chainId: 5,
+    network: Network.ETH_GOERLI,
+  },
+  {
+    chainId: 42,
+    network: Network.ETH_KOVAN,
+  },
+  {
+    chainId: 137,
+    network: Network.MATIC_MAINNET,
+  },
+  {
+    chainId: 80001,
+    network: Network.MATIC_MUMBAI,
+  },
+];
 
 const metadataDirectoryPath = process.env.METADATA_PATH;
 
 const getUserData = async (userId, tokens) => {
+  const metadataStruct = {
+    name: '',
+    description: 'He/She is one of wagumi members.',
+    image: '',
+    external_url: '',
+    tokens: tokens,
+    contributions: [],
+  };
 
-	const metadataStruct = {
-		name: "",
-		description: "He/She is one of wagumi members.",
-		image: "",
-		external_url:"",
-		tokens: tokens,
-		contributions: [],
-	};
+  const request = {
+    database_id: process.env.WAGUMI_USER_DATABASE_ID,
+    filter: {
+      property: 'id',
+      rich_text: {
+        equals: userId,
+      },
+    },
+  };
 
-    const request = { 
-		database_id: process.env.WAGUMI_USER_DATABASE_ID,
-		filter: {
-				property: 'id',
-				rich_text: {
-                    equals: userId
-                }
-		},
-	};
+  const response = await client.databases.query(request);
 
-    const response = await client.databases.query(request);
+  const page = response.results[0];
 
-    const page = response.results[0];
+  let tmp;
 
-    let tmp;
+  tmp = await client.pages.properties.retrieve({
+    page_id: page.id,
+    property_id: page.properties.name.id,
+  });
+  metadataStruct.name = tmp.results[0].title.plain_text;
 
-    tmp = await client.pages.properties.retrieve({ page_id: page.id, property_id: page.properties.name.id});
-    metadataStruct.name = tmp.results[0].title.plain_text;
+  tmp = await client.pages.retrieve({ page_id: page.id });
+  metadataStruct.image = tmp.icon.external.url;
 
-    tmp = await client.pages.retrieve({ page_id: page.id });
-    metadataStruct.image = tmp.icon.external.url;
+  let external_url_id = page.id;
 
-	let external_url_id = page.id
+  const replacedStr = external_url_id.replace(/-/g, '');
 
-	const replacedStr = external_url_id.replace(/-/g, "");
-    
-    metadataStruct.external_url = `https://wagumi-dev.notion.site/${replacedStr}`;
+  metadataStruct.external_url = `https://wagumi-dev.notion.site/${replacedStr}`;
 
-	for(const contribution of contributions) {
-		if(contribution.users.includes(userId)) {
-			metadataStruct.contributions.push(contribution);
-		}
-	}
+  for (const contribution of contributions) {
+    if (contribution.users.includes(userId)) {
+      metadataStruct.contributions.push(contribution);
+    }
+  }
 
-	const json = JSON.stringify(metadataStruct, null, 2);
-	fs.writeFileSync(metadataDirectoryPath + `${userId}.json`, json);
+  const json = JSON.stringify(metadataStruct, null, 2);
+  fs.writeFileSync(metadataDirectoryPath + `${userId}.json`, json);
+};
 
-}
+const userSearch = async () => {
+  const userIds = [];
+  for (const contribution of contributions) {
+    for (user of contribution.users) {
+      if (!userIds.includes(user)) {
+        userIds.push(user);
+      }
+    }
+  }
+  return userIds;
+};
 
-const userSearch = async() => {
-	const userIds = [];
-	for(const contribution of contributions) {
-		for(user of contribution.users) {
-			if(!userIds.includes(user)) {
-				userIds.push(user);
-			}
-		}
-	}
-	return userIds;
-}
-
+// tokens情報取得
+// userIdを引数にとる。EOAとの結びつきを考える
 const getTokenData = async () => {
-	const tokens = [];
-	//Tokenの情報を取ってくる
-	return tokens;
-}
+  if (ownerAddr.length === 42) {
+    for (const chain of chains) {
+      getNFTs(chain, null);
+    }
+  } else {
+    console.log('ウォレットアドレスが共有されていません。');
+  }
+};
+
+const tokens = [];
+
+const getNFTs = async (chain, pageKey) => {
+  //Tokenの情報を取ってくる
+  const settings = {
+    apiKey: process.env.ALCHEMY_API_TOKEN,
+    network: chain.network,
+  };
+  const alchemy = new Alchemy(settings);
+  nftsForOwner = await alchemy.nft.getNftsForOwner(ownerAddr, {
+    pageKey: pageKey,
+  });
+
+  //検索中のチェーンのNFTをtokens配列に格納
+  for (const nft of nftsForOwner.ownedNfts) {
+    // let contract_address = '0x6144d927ee371de7e7f8221b596f3432e7a8e6d9';
+    const data = JSON.parse(
+      fs.readFileSync('src/contractAddressList.json', 'utf-8')
+    );
+
+    for (let i in data.list) {
+      let wagumi_address = data.list[i].constract_address;
+      if (nft.contract.address === wagumi_address) {
+        let token = {
+          address: nft.contract.address,
+          tokenId: nft.tokenId,
+          chainId: chain.chainId,
+        };
+        console.log(token);
+        tokens.push(token);
+      } else {
+        continue;
+      }
+    }
+  }
+
+  if (nftsForOwner.pageKey) {
+    await getNFTs(chain, nftsForOwner.pageKey);
+  } else {
+    return;
+  }
+};
+(async () => {
+  await getTokenData();
+})();
 
 const createMetadata = async () => {
-	const executionMessage = 'create metadata';
-    let executionData;
+  const executionMessage = 'create metadata';
+  let executionData;
 
-	try {
-        executionData = makeExecutionData(executionMessage);
-		const request = { 
-			database_id: process.env.WAGUMI_DATABASE_ID,
-			filter: {
-				property: 'publish',
-				checkbox: {
-					equals: true,
-				}
-			},
-			sorts: [
-				{
-					property: 'last_edited_time',
-					direction: 'descending',
-				},
-			],
-		};
+  try {
+    executionData = makeExecutionData(executionMessage);
+    const request = {
+      database_id: process.env.WAGUMI_DATABASE_ID,
+      filter: {
+        property: 'publish',
+        checkbox: {
+          equals: true,
+        },
+      },
+      sorts: [
+        {
+          property: 'last_edited_time',
+          direction: 'descending',
+        },
+      ],
+    };
 
-		let pages = await client.databases.query(request);
+    let pages = await client.databases.query(request);
 
-		await pushContributionPage(pages);
+    await pushContributionPage(pages);
 
-		while(pages.has_more) {
-			request.start_cursor = pages.next_cursor;
-			pages = await client.databases.query(request)
-			await pushContributionPage(pages)
-		}
-		const userIds = await userSearch();
+    while (pages.has_more) {
+      request.start_cursor = pages.next_cursor;
+      pages = await client.databases.query(request);
+      await pushContributionPage(pages);
+    }
+    const userIds = await userSearch();
 
-		for(let userId of userIds) {
-			//TODO
-			const tokens = await getTokenData(userId);
-			getUserData(userId, tokens);
-		} 
-	fs.writeFileSync("src/executionData.json", executionData);
-
-	}catch(error) {
-		console.error(error);
-	}
-}
+    for (let userId of userIds) {
+      //TODO
+      const tokens = await getTokenData(userId);
+      getUserData(userId, tokens);
+    }
+    fs.writeFileSync('src/executionData.json', executionData);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const pushContributionPage = async (pages) => {
+  for (const page of pages.results) {
+    let tmp;
 
-		for(const page of pages.results){
+    let contribution = {
+      id: '',
+      last_edited_time: '',
+      name: '',
+      image: '',
+      description: '',
+      date: '',
+      users: [],
+    };
 
-			let tmp;
-	
-			let contribution = {
-				id: "",
-				last_edited_time: "",
-				name: "",
-				image: "",
-				description: "",
-				date: "",
-				users: []
-			};
-	
-			//contributeにデータを追加するためのトリガー。falseの場合データ追加をしない。(計算数を減らす目的)
-	
-				
-				contribution.id = page.id;
-				contribution.last_edited_time = page.last_edited_time
-		
-				tmp = await client.pages.properties.retrieve({ page_id: page.id, property_id: page.properties.name.id});
-				contribution.name = tmp.results[0].title.plain_text;
-		
-				tmp = await client.pages.properties.retrieve({ page_id: page.id, property_id: page.properties.image.id});
-				contribution.image = tmp.files[0].name;
-	
-				tmp = await client.pages.properties.retrieve({ page_id: page.id, property_id: page.properties.description.id});
-				contribution.description = tmp.results[0].rich_text.plain_text;
-		
-				tmp = await client.pages.properties.retrieve({ page_id: page.id, property_id: page.properties.date.id});
-				contribution.date = tmp.date;
-	
-				tmp = await client.pages.properties.retrieve({ page_id: page.id, property_id: page.properties.userId.id});
-				contribution.users = tmp.results.map((user) =>{
-						let userId = user.rich_text.plain_text;
-					return userId;
-				});
-				contributions.push(contribution);
-				console.log(contribution)
-	}
+    //contributeにデータを追加するためのトリガー。falseの場合データ追加をしない。(計算数を減らす目的)
 
-	const jsonData = JSON.stringify(contributions,null,2);
-	fs.writeFileSync("src/metadata.json", jsonData);
-// json形式に変換して、ファイル作成
-// コマンドラインの第一引数(discordのユーザーID)をファイル名にする
-}
+    contribution.id = page.id;
+    contribution.last_edited_time = page.last_edited_time;
+
+    tmp = await client.pages.properties.retrieve({
+      page_id: page.id,
+      property_id: page.properties.name.id,
+    });
+    contribution.name = tmp.results[0].title.plain_text;
+
+    tmp = await client.pages.properties.retrieve({
+      page_id: page.id,
+      property_id: page.properties.image.id,
+    });
+    contribution.image = tmp.files[0].name;
+
+    tmp = await client.pages.properties.retrieve({
+      page_id: page.id,
+      property_id: page.properties.description.id,
+    });
+    contribution.description = tmp.results[0].rich_text.plain_text;
+
+    tmp = await client.pages.properties.retrieve({
+      page_id: page.id,
+      property_id: page.properties.date.id,
+    });
+    contribution.date = tmp.date;
+
+    tmp = await client.pages.properties.retrieve({
+      page_id: page.id,
+      property_id: page.properties.userId.id,
+    });
+    contribution.users = tmp.results.map((user) => {
+      let userId = user.rich_text.plain_text;
+      return userId;
+    });
+    contributions.push(contribution);
+    console.log(contribution);
+  }
+
+  const jsonData = JSON.stringify(contributions, null, 2);
+  fs.writeFileSync('src/metadata.json', jsonData);
+  // json形式に変換して、ファイル作成
+  // コマンドラインの第一引数(discordのユーザーID)をファイル名にする
+};
 
 exports.createMetadata = createMetadata;
 exports.userSearch = userSearch;
+exports.getTokenData = getTokenData;
