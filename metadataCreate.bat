caf829f0efabe83d405bc9526d8c7db5d353352a
ikisuke <46296566+ikisuke@users.noreply.github.com>
2022-09-24 12:58:44 +0900

    make bat file

---
+++ metadataCreate.bat
@@ -0,0 +1,13 @@
+@echo off
+cd  %%~dp0: %~dp0
+
+
+  if type "node" > nul 2>&1 (
+	echo "exist!"  
+  ) else (
+      echo "nodeがインストールできていないようです。以下のリンクからインストールしてください \n https://nodejs.org/ja/ "
+  )
+
+npm i
+
+node main.js
