206921257397f8ce1d2364de801434a21753a4b7
ikisuke <46296566+ikisuke@users.noreply.github.com>
2023-08-14 13:21:00 +0900

    update score.json

--- src/lib/external/alchemy.js
+++ src/lib/external/alchemy.js
@@ -4,29 +4,44 @@ const { Network, Alchemy } = require("alchemy-sdk");
 const { env } = require("./dotenv");
 
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
-const settings = {
-  apiKey: env.ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
-  network: Network.MATIC_MAINNET, // Replace with your network.
-};
-
-const alchemy = new Alchemy(settings);
 
 // Print total NFT count returned in the response:
-const getOwnersForNft = async (
+const wagumiSBTOwners = async (
   tokenId,
-  contractAddress = "0xef756b67b90026F91D047D1b991F87D657309A42"
+  contractAddress = "0xef756b67b90026F91D047D1b991F87D657309A42",
+  network = Network.Mainnet
 ) => {
   try {
-    const owners = await alchemy.nft.getOwnersForNft(
-      contractAddress,
-      "495980446245584948"
-    );
+    const settings = {
+      apiKey: env.ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
+      network: network, // Replace with your network.
+    };
+
+    const alchemy = new Alchemy(settings);
+
+    const owners = await alchemy.nft.getOwnersForNft(contractAddress, tokenId);
     console.log(owners);
     return owners.owners[0];
   } catch (error) {
     console.log(error);
   }
 };
+exports.wagumiSBTOwners = wagumiSBTOwners;
 
-exports.getOwnersForNft = getOwnersForNft;
-getOwnersForNft();
+const wagumiCatsOwners = async (contractAddress) => {
+  try {
+    const settings = {
+      apiKey: env.ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
+      network: Network.Mainnet, // Replace with your network.
+    };
+
+    const alchemy = new Alchemy(settings);
+
+    const owners = await alchemy.nft.getOwnersForContract(contractAddress);
+    console.log(owners.owners);
+    return owners.owners;
+  } catch (error) {
+    console.log(error);
+  }
+};
+exports.wagumiCatsOwners = wagumiCatsOwners;
