3a9eb96b144d10c88f9a299923a1a907309bdc91
ikisuke <46296566+ikisuke@users.noreply.github.com>
2023-08-15 22:21:31 +0900

    update .env.sample

--- src/lib/sandbox/getAddress.js
+++ src/lib/sandbox/getAddress.js
@@ -3,19 +3,24 @@ const {
   wagumiSBTOwners,
   wagumiCatsOwners,
 } = require("../../lib/external/alchemy.js");
+const { updateCheckSumAddress } = require("../utils/checksum.js");
+const { updateScore } = require("./api-v2.js");
 
 const main = async () => {
   const addresses = [];
   const ids = getAddressByUserId();
 
   for (let i = 0; i < ids.length; i++) {
-    const address = await wagumiSBTOwners(ids[i]);
+    let address = await wagumiSBTOwners(ids[i]);
+    address = await updateCheckSumAddress(address);
     addresses.push(address);
   }
   // jsonファイルに書き込み
   const fs = require("fs");
   const json = JSON.stringify(addresses);
-  fs.writeFileSync("src/address.json", json);
+  fs.writeFileSync("src/addressHash.json", json);
+
+  await updateScore();
 };
 
 main();
